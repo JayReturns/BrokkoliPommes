6b3e101f855824842067115b6c5068ce273a0a4a
Rafael Simon <rafael.simon091998@gmail.com>
2023-06-25 12:17:41 +0000

    styling

--- client/src/app/components/product-card/product-card.component.ts
+++ client/src/app/components/product-card/product-card.component.ts
@@ -1,21 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Article} from "../../models/article.model";
 import {ShoppingCartService} from "../../services/shopping-cart.service";
-import {MatSelectModule} from '@angular/material/select'
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {CurrencyPipe, TitleCasePipe} from "@angular/common";
-import {MatCardModule} from "@angular/material/card";
-import {MatDividerModule} from "@angular/material/divider";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css'],
-  /*imports: [MatFormFieldModule, MatSelectModule, TitleCasePipe, CurrencyPipe, MatCardModule, MatDividerModule, MatIconModule, MatButtonModule],
-  standalone: true,*/
-
+  styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
 
