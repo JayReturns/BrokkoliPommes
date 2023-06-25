923866168abfabc58b76df4ecd54e4199179cb9e
Rafael Simon <rafael.simon091998@gmail.com>
2023-06-25 14:41:08 +0000

    sql files

---
+++ data/mysql/bp_order_order_positions.sql
@@ -0,0 +1,5 @@
+INSERT INTO brokkolipommes.bp_order_order_positions (order_id, order_positions_id) VALUES ('46d745ca-a37a-482e-869e-98740430c422', 3);
+INSERT INTO brokkolipommes.bp_order_order_positions (order_id, order_positions_id) VALUES ('46d745ca-a37a-482e-869e-98740430c422', 4);
+INSERT INTO brokkolipommes.bp_order_order_positions (order_id, order_positions_id) VALUES ('46d745ca-a37a-482e-869e-98740430c422', 5);
+INSERT INTO brokkolipommes.bp_order_order_positions (order_id, order_positions_id) VALUES ('49b9d630-50dd-43ae-afee-a9bc1217c441', 1);
+INSERT INTO brokkolipommes.bp_order_order_positions (order_id, order_positions_id) VALUES ('49b9d630-50dd-43ae-afee-a9bc1217c441', 2);
